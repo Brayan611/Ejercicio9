b9e160183851b4e8876c26c2261124b381dd3d35
Brayan <brayanlorenzohdez53@gmail.com>
2022-12-13 14:23:07 +0100

    Menú creado

--- Proyecto/Program.cs
+++ Proyecto/Program.cs
@@ -1,2 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int entrada = 0;
+        while (entrada != 5)
+        {
+            Console.WriteLine("Introduzca:\n1 para sumar\n2 para multiplicar\n3 para dividir\n4 para restar\n5 para salir del programa");
+            while (!int.TryParse(Console.ReadLine(), out entrada) || entrada < 0 || entrada > 5)
+                Console.Write("Valor introducido incorrecto\nPor favor introduzca un valor entre 1 y 4: ");
+            if (entrada == 1)
+            {
+
+            }
+            if (entrada == 2)
+            {
+
+            }
+            if (entrada == 3)
+            {
+
+            }
+            if (entrada == 4)
+            {
+
+            }
+        }
+        
+    }
+}
